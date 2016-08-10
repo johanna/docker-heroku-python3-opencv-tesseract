a72d774c42baf018b33f5b2c9d52e974e91b0290
supistar <supistars@gmail.com>
2016-08-10 21:58:06 +0900

    Fix leptonica installation script to place under `/app/.heroku/leptonica`.

--- scripts/install_leptonica.sh
+++ scripts/install_leptonica.sh
@@ -1,16 +1,25 @@
 #!/bin/sh
 
-# Prepare
+# Variables
 VERSION=1.73
 NAME=leptonica-${VERSION}
+PREFIX=/app/.heroku/leptonica
+PROFILE=/app/.profile.d/leptonica.sh
+
+# Prepare
 wget http://www.leptonica.org/source/${NAME}.tar.gz
 tar xvf ${NAME}.tar.gz
 cd ${NAME}
 
 # Build
-./configure
+mkdir ${PREFIX}
+./configure --prefix=${PREFIX}
 make
 checkinstall
 ldconfig
 cd -
+
+# Prepare profile
+echo 'export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/app/.heroku/leptonica/lib/pkgconfig' > ${PROFILE}
+echo 'export PATH=$PATH:/app/.heroku/leptonica/bin' >> ${PROFILE}
 
